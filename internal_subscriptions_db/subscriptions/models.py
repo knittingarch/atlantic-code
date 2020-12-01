b75d758cca4b904a2fc82d54c27a4213c4b816bf
knittingarch <knittingarch@gmail.com>
2020-12-01 18:23:50 -0500

    Create Database Schema
    Based on the post request the future API endpoint will consume,
    this commit creates Customer, Subscription, and Gift models. Each model's
    fields also conform to the sample post request.  From the example, it
    appears that a Customer can have one Subscription, but many Gift
    subscriptions.  As a result, I have set up foreign key associations on
    the Subscription and Gift models.
    
    In Ruby, it is possible to have a `belongs_to` association, where I
    could specify that a Customer belongs to a Subscription while a
    Subscription has many Customers, but I may not need that level of
    specificity here. I will revisit if my assumption is incorrect.

--- internal_subscriptions_db/subscriptions/models.py
+++ internal_subscriptions_db/subscriptions/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+  first_name = models.CharField(max_length = 200)
+  last_name = models.CharField(max_length = 200)
+  address_1 = models.CharField(max_length = 300)
+  address_2 = models.CharField(max_length = 300)
+  city = models.CharField(max_length = 200)
+  state = models.CharField(max_length = 200)
+  postal_code = models.CharField(max_length = 20)
+
+  def __str__(self):
+    return (self.first_name + " " + self.last_name)
+
+class Subscription(models.Model):
+  customer = models.ForeignKey(
+    Customer,
+    null=True,
+    blank=True,
+    on_delete=models.CASCADE
+  )
+  plan_name = models.CharField(max_length = 300)
+  price = models.CharField(max_length = 20)
+
+  def __str__(self):
+    return self.plan_name
+
+class Gift(models.Model):
+  customer = models.ForeignKey(
+    Customer,
+    null=True,
+    blank=True,
+    on_delete=models.CASCADE
+  )
+  plan_name = models.CharField(max_length = 300)
+  price = models.CharField(max_length = 20)
+  recipient_email = models.CharField(max_length = 100)
+
+  def __str__(self):
+    return self.plan_name
