f6a9ba939d9515ecf4ccf1d8f410383c9de5cc08
knittingarch <knittingarch@gmail.com>
2020-12-01 22:44:01 -0500

    Add uniqueness validation and remove spaces

--- internal_subscriptions_db/subscriptions/models.py
+++ internal_subscriptions_db/subscriptions/models.py
@@ -1,29 +1,16 @@
 from django.db import models
 
 class Customer(models.Model):
-  first_name = models.CharField(max_length = 200)
-  last_name = models.CharField(max_length = 200)
-  address_1 = models.CharField(max_length = 300)
-  address_2 = models.CharField(max_length = 300)
-  city = models.CharField(max_length = 200)
-  state = models.CharField(max_length = 200)
-  postal_code = models.CharField(max_length = 20)
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  address_1 = models.CharField(max_length=300)
+  address_2 = models.CharField(max_length=300)
+  city = models.CharField(max_length=200)
+  state = models.CharField(max_length=200)
+  postal_code = models.CharField(max_length=20)
 
   def __str__(self):
     return (self.first_name + " " + self.last_name)
-
-class Subscription(models.Model):
-  customer = models.ForeignKey(
-    Customer,
-    null=True,
-    blank=True,
-    on_delete=models.CASCADE
-  )
-  plan_name = models.CharField(max_length = 300)
-  price = models.CharField(max_length = 20)
-
-  def __str__(self):
-    return self.plan_name
 
 class Gift(models.Model):
   customer = models.ForeignKey(
@@ -32,9 +19,22 @@ class Gift(models.Model):
     blank=True,
     on_delete=models.CASCADE
   )
-  plan_name = models.CharField(max_length = 300)
-  price = models.CharField(max_length = 20)
-  recipient_email = models.CharField(max_length = 100)
+  plan_name = models.CharField(max_length=300)
+  price = models.CharField(max_length=20)
+  recipient_email = models.CharField(max_length=100, unique=True)
+
+  def __str__(self):
+    return self.plan_name
+
+class Subscription(models.Model):
+  customer = models.ForeignKey(
+    Customer,
+    null=True,
+    blank=True,
+    on_delete=models.CASCADE
+  )
+  plan_name = models.CharField(max_length=300)
+  price = models.CharField(max_length=20)
 
   def __str__(self):
     return self.plan_name
