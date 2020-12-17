872ab8f8a8c8d9562c78a0c8dbb12461c49def06
knittingarch <knittingarch@gmail.com>
2020-12-17 09:45:16 -0500

    Add Tests For API Requests
    This commit adds tests for the GET and POST actions for each API
    request.  It also adds a `basename` to each viewset to facilitate
    reversing the URL.
    
    Unfortunately, creating a customer with a gift still eluds me, but all
    other cases are working as expected.

--- internal_subscriptions_db/subscriptions/tests.py
+++ internal_subscriptions_db/subscriptions/tests.py
@@ -1,3 +1,147 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .factories import CustomerFactory, GiftFactory, SubscriptionFactory
+from .models import Customer, Gift, Subscription
+
+class GiftTestCases(APITestCase):
+    def setUp(self):
+        self.gift = GiftFactory()
+
+    def tearDown(self):
+        self.gift.delete()
+
+    def test_gift_GET_request(self):
+        """
+        Ensure we can retrieve a Gift object.
+        """
+
+        url = reverse('gift-list')
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Gift.objects.get().plan_name, self.gift.plan_name)
+
+    def test_gift_POST_request(self):
+        """
+        Ensure we can create a new Gift object.
+        """
+
+        url = reverse('gift-list')
+        data = {
+            'plan_name': 'Test Gift',
+            'price': '9999',
+            'recipient_email': 'test@test.com',
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Gift.objects.count(), 2)
+        self.assertEqual(
+            Gift.objects.filter(plan_name='Test Gift').first().plan_name,
+            'Test Gift'
+        )
+
+class SubscriptionTestCases(APITestCase):
+    def setUp(self):
+        self.subscription = SubscriptionFactory()
+
+    def tearDown(self):
+        self.subscription.delete()
+
+    def test_subscription_GET_request(self):
+        """
+        Ensure we can retrieve a Subscription object.
+        """
+
+        url = reverse('subscription-list')
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Subscription.objects.get().plan_name, self.subscription.plan_name)
+
+    def test_subscription_POST_request(self):
+        """
+        Ensure we can create a new Subscription object.
+        """
+
+        url = reverse('subscription-list')
+        data = {
+            'plan_name': 'Test Subscription',
+            'price': '9999',
+            'recipient_email': 'test@test.com',
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Subscription.objects.count(), 2)
+        self.assertEqual(
+            Subscription.objects.filter(plan_name='Test Subscription').first().plan_name,
+            'Test Subscription'
+        )
+
+class CustomerTestCases(APITestCase):
+    def test_get_customer(self):
+        """
+        Ensure we can retrieve an existing Customer object.
+        """
+        CustomerFactory()
+
+        url = reverse('customer-list')
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Customer.objects.get().first_name, 'Betty')
+        self.assertEqual(Customer.objects.get().last_name, 'Boop')
+
+    def test_create_customer(self):
+        """
+        Ensure we can create a Customer object.
+        """
+
+        data = {
+            'first_name': 'Beatrice',
+            'last_name': 'Potter',
+            'address_1': '123 Washington Street',
+            'city': 'Milwaukee',
+            'state': 'WI',
+            'postal_code': '53212',
+            'gifts': [],
+            'subscriptions': [],
+        }
+
+        url = reverse('customer-list')
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Customer.objects.count(), 1)
+        self.assertEqual(Customer.objects.get().first_name, 'Beatrice')
+        self.assertEqual(Customer.objects.get().last_name, 'Potter')
+
+
+    def test_create_customer_with_gift(self):
+        """
+        Ensure we can create a Customer object with a gift.
+        """
+
+        gift = GiftFactory()
+
+        data = {
+            'first_name': 'Beatrice',
+            'last_name': 'Potter',
+            'address_1': '123 Washington Street',
+            'city': 'Milwaukee',
+            'state': 'WI',
+            'postal_code': '53212',
+            'gifts': [gift],
+            'subscriptions': [],
+        }
+
+        url = reverse('customer-list')
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Customer.objects.count(), 1)
+        self.assertEqual(Customer.objects.get().first_name, 'Beatrice')
+        self.assertEqual(Customer.objects.get().last_name, 'Potter')
--- internal_subscriptions_db/subscriptions/urls.py
+++ internal_subscriptions_db/subscriptions/urls.py
@@ -3,9 +3,9 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'customers', views.CustomerViewSet)
-router.register(r'gifts', views.GiftViewSet)
-router.register(r'subscriptions', views.SubscriptionViewSet)
+router.register(r'customers', views.CustomerViewSet, basename='customer')
+router.register(r'gifts', views.GiftViewSet, basename='gift')
+router.register(r'subscriptions', views.SubscriptionViewSet, basename='subscription')
 
 urlpatterns = [
   path('', include(router.urls)),
